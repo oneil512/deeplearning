532b6800bcc47995b4d52021255782d20e4e3693
Clay O'Neil <clay@wewandr.com>
2019-02-16 12:50:45 -0600

    finish back prop for weights

--- feedforward.py
+++ feedforward.py
@@ -7,7 +7,9 @@ class feedforward:
         self.softmax = []
         self.loss = 0
         self.activations = []
+        self.learningRate = .01
         self.outputs = []
+        self.leak = -1 * 0.001
         weights = []
         bias = []
         for index, i in enumerate(dim[:-1]):
@@ -19,6 +21,7 @@ class feedforward:
     # x is np array
     def train(self, x):
         cur = x
+        self.activations.append(cur)
         for index, layer in enumerate(self.weights[:-1]):
             cur = np.matmul(layer, cur) + self.bias[index]
             self.outputs.append(cur)
@@ -30,7 +33,7 @@ class feedforward:
 
     # v is a np vector
     def leakyRelu(self, v):
-        l_relu = lambda x: max([.001 * x, x])
+        l_relu = lambda x: max([self.leak * x, x])
         v_relu = np.vectorize(l_relu)
         return v_relu(v)
 
@@ -38,31 +41,43 @@ class feedforward:
         vals = [math.exp(i) for i in v]
         s = np.sum(vals)
         self.softmax = [math.exp(i) / s for i in v]
+        return self.softmax
 
     def logLoss(self, gt, predict):
         self.loss = -1 * np.log(np.dot(gt, predict))
+        return self.loss
 
-    def bProp(self, less, gt, predict):
+    def bProp(self, loss, gt, predict):
         updates = []
-        updates.append(self.gradDescSoftLog())
-        for index, weight in enumerate(reversed(self.weights[:-1]])):
-            updates.append(gradDesc(weight, updates[-1], self.activations[-(index + 1)]))
+        updates.append(self.gradDescSoftLog(self.weights[-1], gt, self.activations[-1]))
+        for index, weight in enumerate(reversed(self.weights[:-1])):
+            updates.append(self.gradDesc(weight, updates[-1], self.activations[-(index + 1)], self.outputs[-(index + 1)]))
+        for i, update in enumerate(reversed(updates)):
+            self.updateWeights(self.weights[i], update)
 
 
-    def gradDesc(self, weight, prevGrads, activations):
-        gradients = np.empty(shape=weight.shape)
-       #sum down the columns 
-        for i in gradients:
+    def gradDesc(self, weight, prevGrads, activations, outputs):
+        w_gradients = np.empty(shape=weight.shape)
+        b_gradients = np.empty(shape=weight.shape)
+        m_sum = np.sum(prevGrads, axis=0)
+        for i_index, i in enumerate(w_gradients):
+            for j_index, j in enumerate(i):
+                d_activation = activations[i_index] if activations[i_index] > 0 else activations[i_index] * self.leak
+                j = d_activation * m_sum[i_index] * outputs[i_index]
+        return w_gradients
+
+    def updateWeights(self, weights, updates):
+        for i_index, i in enumerate(updates):
+            for j_index, j in enumerate(i):
+                weights[i_index][j_index] += self.learningRate * j
 
 
-
-    def gradDescSoftLog(self):
-        for i in self.weights[-1]:
-            for j in i:
-                gradients[i][j] = (self.softmax[i] - gt[i]) * predict[j]
-        return gradients
-
-
+    def gradDescSoftLog(self, weight, gt, activations):
+        w_gradients = np.empty(shape=weight.shape)
+        for i_index, i in enumerate(w_gradients):
+            for j_index, j in enumerate(i):
+                j = (self.softmax[i_index] - gt[i_index]) * activations[j_index]
+        return w_gradients
 
     def printShape(self):
         for weight in self.weights:
@@ -71,15 +86,13 @@ class feedforward:
             print(i.shape)
 
 shape = [6, 4, 2]
-print('shape ' + str(shape))
 net = feedforward(shape)
-net.printShape()
+print('prior weights', net.weights)
 x = np.random.rand(6)
 y = np.array([1,0])
+print('train data', x)
+print('gt', y)
 res = net.train(x)
-print(x)
-print(y)
-print(res)
-l = net.logLoss(y, res)
-print(l)
-
+loss = net.logLoss(y, res)
+net.bProp(loss, y, res)
+print('post weights', net.weights)
